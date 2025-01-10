e1109dded357c7ab81936c8c5a1255d9d4eee2a4
Nitesh Kumar <79629635+Nitesh600@users.noreply.github.com>
2025-01-10 18:40:47 +0530

    Update SignUp.js
    add dashboard button instead of signup

--- src/landing_page/signup/SignUp.js
+++ src/landing_page/signup/SignUp.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function SignUp() {
     return ( 
-        <Link to={"https://zerodha-dashboard-phi.vercel.app/holdings"}><button>signup</button></Link>
+        <Link to={"https://zerodha-dashboard-phi.vercel.app/holdings"}><button className='p-2 btn btn-primary fs-5 mb-5 mt-4' type="button" style={{width: "20%", margin: "0 auto" }}>Dashboard </button></Link>
         
      );
 }
