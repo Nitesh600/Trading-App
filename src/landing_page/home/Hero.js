22caf0a55521694388d0bf6f5641a17c936590bc
Nitesh Kumar <79629635+Nitesh600@users.noreply.github.com>
2025-01-24 16:32:56 +0530

    Update Hero.js

--- src/landing_page/home/Hero.js
+++ src/landing_page/home/Hero.js
@@ -8,7 +8,10 @@ function Hero() {
          <img src='media\homeHero.png' alt='Hero Image' className='mb-5'/>
          <h1 className='mt-5 ml-10'>Invest in everything</h1>
          <p className='fs-5'>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
-         <button className='p-2 btn btn-primary fs-5 mb-5 mt-5' type="button" style={{width: "20%", margin: "0 auto" }}>Signup now</button>
+        <Link to= "https://auth-client-nine.vercel.app/signup">
+        <button className='p-2 btn btn-primary fs-5 mb-5 mt-5' type="button" style={{width: "20%", margin: "0 auto" }}>Signup now</button>
+        </Link>
+       
 
         </div>
 
